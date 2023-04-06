8a2d67059e55d2c2bc5699e4ab0828072fca8b8a
Mathis Boulais <mathis.oxym@gmail.com>
2023-04-06 12:39:44 +0200

    finishgame

--- main.py
+++ main.py
@@ -1,139 +1,152 @@
 import pygame
+import random
 import cv2
 
-# Initialisation de Pygame
+# Initialisation de pygame
 pygame.init()
 
-# Taille de l'écran
-screen_width = 800
-screen_height = 600
+# Définition des variables de jeu
+WIDTH = 800
+HEIGHT = 600
+BALL_RADIUS = 10
+PADDLE_WIDTH = 100
+PADDLE_HEIGHT = 10
+BLOCK_WIDTH = 70
+BLOCK_HEIGHT = 20
+BLOCK_MARGIN = 10
+BLOCK_ROWS = 5
+BLOCK_COLS = 10
+BLOCK_COLORS = [(255, 0, 0), (255, 128, 0), (255, 255, 0), (128, 255, 0), (0, 255, 0)]
 
-# Initialisation de la fenêtre Pygame
-screen = pygame.display.set_mode((screen_width, screen_height))
+# Création de la fenêtre de jeu
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Breakout")
 
-block_colors = [(69, 177, 232), (242, 85, 96), (86, 174, 87)]
-block_size = 65
-block_spacing = 5
-blocks_per_row = screen_width // (block_size + block_spacing)
+# Définition des fonctions
+def draw_ball(ball_x, ball_y):
+    pygame.draw.circle(screen, (255, 255, 255), (int(ball_x), int(ball_y)), BALL_RADIUS)
 
-# Chargement de l'image de l'objet principal
-platbb_img = pygame.image.load('images/platbb.png')
+def draw_paddle(paddle_x):
+    pygame.draw.rect(screen, (255, 255, 255), (int(paddle_x), HEIGHT - PADDLE_HEIGHT, PADDLE_WIDTH, PADDLE_HEIGHT))
 
-# Position initiale de l'objet principal
-platbb_x = screen_width / 2
-platbb_y = screen_height -80
-platbb_img = pygame.transform.scale(platbb_img, (100, 50))
+def draw_block(block_x, block_y, block_color):
+    pygame.draw.rect(screen, block_color, (int(block_x), int(block_y), BLOCK_WIDTH, BLOCK_HEIGHT))
 
-# Initialisation de la balle
-ball_x = screen_width / 2
-ball_y = screen_height / 2
-ball_speed_x = 1
-ball_speed_y = 1
-ball_radius = 10
+def collision(ball_x, ball_y, paddle_x, blocks):
+    # Vérification de la collision avec le paddle
+    if ball_y + BALL_RADIUS >= HEIGHT - PADDLE_HEIGHT and ball_x >= paddle_x and ball_x <= paddle_x + PADDLE_WIDTH:
+        return True
+    # Vérification de la collision avec les blocs
+    for row in range(BLOCK_ROWS):
+        for col in range(BLOCK_COLS):
+            block = blocks[row][col]
+            if block is not None:
+                block_x, block_y, block_color = block
+                if ball_x + BALL_RADIUS >= block_x and ball_x - BALL_RADIUS <= block_x + BLOCK_WIDTH and ball_y + BALL_RADIUS >= block_y and ball_y - BALL_RADIUS <= block_y + BLOCK_HEIGHT:
+                    # Disparition du block touché
+                    blocks[row][col] = None
+                    return True
+    return False
 
-# Allumer la caméra
+# Création des blocs
+blocks = []
+for row in range(BLOCK_ROWS):
+    blocks.append([])
+    for col in range(BLOCK_COLS):
+        block_x = col * (BLOCK_WIDTH + BLOCK_MARGIN) + BLOCK_MARGIN
+        block_y = row * (BLOCK_HEIGHT + BLOCK_MARGIN) + BLOCK_MARGIN
+        block_color = BLOCK_COLORS[row]
+        blocks[row].append((block_x, block_y, block_color))
+
+# Boucle principale du jeu
+ball_x = WIDTH / 2
+ball_y = HEIGHT / 2
+ball_speed_x = random.choice([-2, 5])
+ball_speed_y = 2
+paddle_x = WIDTH / 2 - PADDLE_WIDTH / 2
+paddle_speed = 0
+
 cap = cv2.VideoCapture(0)
+x = 0
+w = 0
 
-# Charger le fichier XML contenant les informations sur les visages
+# charger le fichier XML contenant les informations sur les visages
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
 
-# Boucle principale
-while True:
-    # Capturer une image depuis la caméra
+running = True
+while running:
+    
     ret, frame = cap.read()
+    resize_frame = cv2.resize(frame, (WIDTH//2, HEIGHT//2))
 
-    # Redimensionner l'image de la caméra
-    resized_frame = cv2.resize(frame, (screen_width // 4, screen_height // 4))
-
-    # Convertir l'image en noir et blanc
+    # convertir l'image en noir et blanc
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    # Détecter les visages dans l'image
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-    screen.fill((255, 255, 255))
-    
-    # le joueur ne peut pas sortir de l'écran
-    if platbb_x < 0:
-        platbb_x = 0
-    if platbb_x > screen_width - 100:
-        platbb_x = screen_width - 100
-    
-    # Dessiner un rectangle autour de chaque visage détecté
+    # détecter les visages dans l'image
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)    
+
+    # dessiner un rectangle autour de chaque visage détecté
     for (x,y,w,h) in faces:
         cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
-
-        # Detecter si ma tete est vers la gauche ou vers la droite
+        
+# detecte si ma tete est vers la gauche ou vers la droite
         roi_gray = gray[y:y+h, x:x+w]
         roi_color = frame[y:y+h, x:x+w]
         eyes = eye_cascade.detectMultiScale(roi_gray)
         for (ex,ey,ew,eh) in eyes:
             cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
-
-        # Déplacer l'objet principal en fonction du mouvement de la tête
-        if x + w/2 < frame.shape[1] / 2:
-            platbb_x += 7
-        else:
-            platbb_x -= 7
-            
-        if pygame.key.get_pressed()[pygame.K_LEFT]:
-            platbb_x -= 7
-        if pygame.key.get_pressed()[pygame.K_RIGHT]:
-            platbb_x += 7
-            
-
-    for i in range(0, blocks_per_row):
-        for j in range(0, len(block_colors)):
-            x = i * (block_size + block_spacing)
-            y = j * (block_size + block_spacing)
-            pygame.draw.rect(screen, block_colors[j], (x, y, block_size, 20))
-            
-    ball_x += ball_speed_x
-    ball_y += ball_speed_y
-    # Rebondir sur les murs horizontaux
-    if ball_y - ball_radius < 0 or ball_y + ball_radius > screen_height:
-        ball_speed_y = -ball_speed_y
-
-    # Rebondir sur les murs verticaux
-    if ball_x - ball_radius < 0 or ball_x + ball_radius > screen_width:
-        ball_speed_x = -ball_speed_x
-
-    # Rebondir sur la plateforme
-    if ball_y + ball_radius > platbb_y and ball_x + ball_radius > platbb_x and ball_x - ball_radius < platbb_x + 100:
-        ball_speed_y = -ball_speed_y
-        
-    for i in range(0, blocks_per_row):
-        for j in range(0, len(block_colors)):
-            # supprime le seul block toucher et pas toute la rangée en sortant de la boucle
-            if ball_y + ball_radius > j * (block_size + block_spacing) and ball_y + ball_radius < j * (block_size + block_spacing) + 20 and ball_x + ball_radius > i * (block_size + block_spacing) and ball_x - ball_radius < i * (block_size + block_spacing) + block_size:
-                ball_speed_y = -ball_speed_y
-                break
-            x = i * (block_size + block_spacing)
-            y = j * (block_size + block_spacing)
-                
-
-        
-# si la ball touche le bas de l'écran
-    if ball_y + ball_radius > screen_height:
-            pygame.quit()        
-
-    # Afficher la balle
-    pygame.draw.circle(screen, (0, 0, 0), (int(ball_x), int(ball_y)), 5)
-
-
-    screen.blit(platbb_img, (platbb_x, platbb_y))
-
-    cv2.imshow('frame', resized_frame)
-
-    cv2.moveWindow('frame', 0, 0)
-
-    # Rafraîchir l'écran
-    pygame.display.flip()
-
-    # Attendre une touche pour quitter
+    
+    
+    # Gestion des événements
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            pygame.quit()
-            cap.release()
-            cv2.destroyAllWindows()
-            exit()
+            running = False
+    
+    if x + w/2 < frame.shape[1]/2:
+        paddle_speed = 5
+    elif x + w/2 > frame.shape[1]/2:
+        paddle_speed = -5
+                
+    cv2.imshow('frame',resize_frame)
+
+
+    # Mise à jour de la position du paddle
+    paddle_x += paddle_speed
+    if paddle_x < 0:
+        paddle_x = 0
+    elif paddle_x > WIDTH - PADDLE_WIDTH:
+        paddle_x = WIDTH - PADDLE_WIDTH 
+
+    # Mise à jour de la position de la balle
+    ball_x += ball_speed_x
+    ball_y += ball_speed_y
+    if ball_x < BALL_RADIUS or ball_x > WIDTH - BALL_RADIUS:
+        ball_speed_x *= -1
+    if ball_y < BALL_RADIUS:
+        ball_speed_y *= -1
+    elif ball_y > HEIGHT - BALL_RADIUS:
+        running = False
+
+    # Vérification de la collision
+    if collision(ball_x, ball_y, paddle_x, blocks):
+        ball_speed_y *= -1
+        
+# place la camera en bas a gauche
+    cv2.moveWindow('frame', 0, 0)
+    
+    # Affichage
+    screen.fill((0, 0, 0))
+    draw_ball(ball_x, ball_y)
+    draw_paddle(paddle_x)
+    for row in range(BLOCK_ROWS):
+        for col in range(BLOCK_COLS):
+            block = blocks[row][col]
+            if block is not None:
+                block_x, block_y, block_color = block
+                draw_block(block_x, block_y, block_color)
+    pygame.display.flip()
+cap.release()
+cv2.destroyAllWindows()
+pygame.quit()
+    
